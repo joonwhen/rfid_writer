742f8a16157211899250ea5208659589ae024836
Jun Wen <junwen.toh@ncs.com.sg>
2021-03-01 17:03:25 +0800

    Deleted unwanted GUI elements

--- WindowsApplication1/Form1.Designer.cs
+++ WindowsApplication1/Form1.Designer.cs
@@ -30,18 +30,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.COM = new System.Windows.Forms.Label();
             this.button30 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lbl_epc = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
@@ -117,16 +111,6 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(680, 28);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 41);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "WriteTag";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button6
             // 
             this.button6.Enabled = false;
@@ -191,89 +175,6 @@
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ActiveMode";
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(652, 254);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 41);
-            this.button4.TabIndex = 24;
-            this.button4.Text = "ReadRFPower";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(652, 230);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 41);
-            this.button5.TabIndex = 25;
-            this.button5.Text = "SetRFPower";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(602, 23);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(83, 41);
-            this.button8.TabIndex = 26;
-            this.button8.Text = "RelayOn";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(571, 23);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(83, 41);
-            this.button9.TabIndex = 27;
-            this.button9.Text = "RelayOff";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox2.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25",
-            "26",
-            "27",
-            "28",
-            "29",
-            "30"});
-            this.comboBox2.Location = new System.Drawing.Point(555, 245);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(69, 27);
-            this.comboBox2.TabIndex = 28;
             // 
             // timer1
             // 
@@ -423,7 +324,7 @@
             this.bn_CheckOut.Name = "bn_CheckOut";
             this.bn_CheckOut.Size = new System.Drawing.Size(188, 23);
             this.bn_CheckOut.TabIndex = 41;
-            this.bn_CheckOut.Text = "Checkout!";
+            this.bn_CheckOut.Text = "Checkout";
             this.bn_CheckOut.UseVisualStyleBackColor = true;
             this.bn_CheckOut.Click += new System.EventHandler(this.bn_CheckOut_Click);
             // 
@@ -492,7 +393,7 @@
             this.bn_AddItem.Name = "bn_AddItem";
             this.bn_AddItem.Size = new System.Drawing.Size(89, 23);
             this.bn_AddItem.TabIndex = 46;
-            this.bn_AddItem.Text = "Add Item!";
+            this.bn_AddItem.Text = "Add Item";
             this.bn_AddItem.UseVisualStyleBackColor = true;
             this.bn_AddItem.Click += new System.EventHandler(this.bn_AddItem_Click);
             // 
@@ -500,16 +401,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(799, 426);
+            this.ClientSize = new System.Drawing.Size(543, 432);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox3);
             this.Name = "Form1";
@@ -532,18 +427,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label COM;
         private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Button button30;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lbl_epc;
         private System.Windows.Forms.GroupBox groupBox4;
